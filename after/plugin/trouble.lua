aeacc2a7ce369e55b53c0a13be0b6ebc5fbb969c
ThiiagoAC7 <thiiagooac7@gmail.com>
2023-02-23 19:44:07 -0300

    feat(trouble): update next and previous keys

--- after/plugin/trouble.lua
+++ after/plugin/trouble.lua
@@ -1,5 +1,11 @@
 local trouble = require('trouble')
-trouble.setup()
+
+trouble.setup({
+    action_keys = {
+        previous = "l",
+        next = "k"
+    }
+})
 
 
 vim.keymap.set('n','<leader>t','<cmd>TroubleToggle<cr>')
