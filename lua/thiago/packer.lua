e6f8f9b4f3fe7e23cda9e72cfa1cba0c2170fe38
ThiiagoAC7 <thiiagooac7@gmail.com>
2023-02-24 13:14:19 -0300

    feat: adding nvim-colorizer plugin

--- lua/thiago/packer.lua
+++ lua/thiago/packer.lua
@@ -17,61 +17,63 @@ return require('packer').startup(function(use)
         'folke/tokyonight.nvim',
         as = 'tokyonight',
         config = function()
-                vim.cmd('colorscheme tokyonight')
-            end
-        })
+            vim.cmd('colorscheme tokyonight')
+        end
+    })
 
-        -- treesitter
-        use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-        -- use('ThePrimeagen/harpoon')
+    -- treesitter
+    use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    -- use('ThePrimeagen/harpoon')
 
-        -- git stuff
-        use('tpope/vim-fugitive')
-        use('lewis6991/gitsigns.nvim')
+    -- git stuff
+    use('tpope/vim-fugitive')
+    use('lewis6991/gitsigns.nvim')
 
-        use('nvim-lualine/lualine.nvim') -- Fancier statusline
-        use('windwp/nvim-autopairs')
-        use('windwp/nvim-ts-autotag')
-        use('akinsho/nvim-bufferline.lua')
+    use('nvim-lualine/lualine.nvim') -- Fancier statusline
+    use('windwp/nvim-autopairs')
+    use('windwp/nvim-ts-autotag')
+    use('akinsho/nvim-bufferline.lua')
 
-        use('nvim-tree/nvim-web-devicons')
+    use('nvim-tree/nvim-web-devicons')
 
-        -- autocomplete / lsp
-        use('onsails/lspkind-nvim') -- vscode-like pictograms
-        use {
-            'VonHeikemen/lsp-zero.nvim',
-            branch = 'v1.x',
-            requires = {
-                -- LSP Support
-                {'neovim/nvim-lspconfig'},             -- Required
-                {'williamboman/mason.nvim'},           -- Optional
-                {'williamboman/mason-lspconfig.nvim'}, -- Optional
+    -- autocomplete / lsp
+    use('onsails/lspkind-nvim') -- vscode-like pictograms
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v1.x',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {'williamboman/mason.nvim'},           -- Optional
+            {'williamboman/mason-lspconfig.nvim'}, -- Optional
 
-                -- Autocompletion
-                {'hrsh7th/nvim-cmp'},         -- Required
-                {'hrsh7th/cmp-nvim-lsp'},     -- Required
-                {'hrsh7th/cmp-buffer'},       -- Optional
-                {'hrsh7th/cmp-path'},         -- Optional
-                {'saadparwaiz1/cmp_luasnip'}, -- Optional
-                {'hrsh7th/cmp-nvim-lua'},     -- Optional
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},         -- Required
+            {'hrsh7th/cmp-nvim-lsp'},     -- Required
+            {'hrsh7th/cmp-buffer'},       -- Optional
+            {'hrsh7th/cmp-path'},         -- Optional
+            {'saadparwaiz1/cmp_luasnip'}, -- Optional
+            {'hrsh7th/cmp-nvim-lua'},     -- Optional
 
-                -- Snippets
-                {'L3MON4D3/LuaSnip'},             -- Required
-                {'rafamadriz/friendly-snippets'}, -- Optional
-            }
+            -- Snippets
+            {'L3MON4D3/LuaSnip'},             -- Required
+            {'rafamadriz/friendly-snippets'}, -- Optional
         }
+    }
 
-        -- error messages
-        use("folke/trouble.nvim")
-        use('numToStr/Comment.nvim')
+    -- error messages
+    use("folke/trouble.nvim")
+    use('numToStr/Comment.nvim')
 
-        -- use('jose-elias-alvarez/null-ls.nvim')
-        
-        -- markdown preview
-        use({
-            "iamcco/markdown-preview.nvim",
-            run = function() vim.fn["mkdp#util#install"]() end,
-        })
+    -- use('jose-elias-alvarez/null-ls.nvim')
+
+    -- markdown preview
+    use({
+        "iamcco/markdown-preview.nvim",
+        run = function() vim.fn["mkdp#util#install"]() end,
+    })
+
+    use('norcalli/nvim-colorizer.lua')
 
 end)
 
