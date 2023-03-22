b03bbf434ada9e5467a11b37f1f2beabdbc81770
ThiiagoAC7 <thiiagooac7@gmail.com>
2023-03-22 20:34:48 -0300

    fix(term): toggleterm working on wsl

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -1,16 +1,17 @@
 local function _shell()
-	if vim.fn.has("win32") then
-		return "pwsh"
-	end
-	return vim.o.shell
+    if vim.fn.has("win32") and not vim.fn.has("wsl") then
+        print("adjaskldajsçdklasç")
+        return "pwsh"
+    end
 end
 
 require("toggleterm").setup({
-	shell = _shell(),
-	open_mapping = "<C-t>",
-	direction = "float",
-	float_opts = {
-		border = "rounded",
-	},
+    shell = _shell(),
+    open_mapping = "<C-t>",
+    direction = "float",
+    float_opts = {
+        border = "rounded",
+    },
     close_on_exit = true,
 })
+
