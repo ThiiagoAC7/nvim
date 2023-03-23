7baccb3ca143d389efca335b5ead67a25b1e5a82
ThiiagoAC7 <thiiagooac7@gmail.com>
2023-03-22 21:14:59 -0300

    fix(term): toggleterm now actually working in pwsh and wsl

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -1,17 +1,15 @@
 local function _shell()
-    if vim.fn.has("win32") and not vim.fn.has("wsl") then
-        print("adjaskldajsçdklasç")
-        return "pwsh"
-    end
+	if vim.loop.os_uname().sysname == "Windows_NT" then
+		return "pwsh"
+	end
 end
 
 require("toggleterm").setup({
-    shell = _shell(),
-    open_mapping = "<C-t>",
-    direction = "float",
-    float_opts = {
-        border = "rounded",
-    },
-    close_on_exit = true,
+	shell = _shell(),
+	open_mapping = "<C-t>",
+	direction = "float",
+	float_opts = {
+		border = "rounded",
+	},
+	close_on_exit = true,
 })
-
--- lua/thiago/set.lua
+++ lua/thiago/set.lua
@@ -24,17 +24,17 @@ vim.opt.signcolumn = "yes"
 vim.opt.wrap = false
 
 -- setting powershell as default shell for windows
-if vim.fn.has("win32") and not vim.fn.has("wsl") then
-    local powershell_options = {
-        shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell",
-        shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
-        shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
-        shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
-        shellquote = "",
-        shellxquote = "",
-    }
+if vim.loop.os_uname().sysname == "Windows_NT" then
+	local powershell_options = {
+		shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell",
+		shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+		shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+		shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+		shellquote = "",
+		shellxquote = "",
+	}
 
-    for option, value in pairs(powershell_options) do
-        vim.opt[option] = value
-    end
+	for option, value in pairs(powershell_options) do
+		vim.opt[option] = value
+	end
 end
